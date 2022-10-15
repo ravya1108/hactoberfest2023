f7550cd37d062f59e7f0b64e501dd9b99ad3f643
Kunnu72 <94804624+Kunnu72@users.noreply.github.com>
2022-10-15 12:58:32 +0530

    ROCK PAPER SCISSORS ADDED IN PYTHON

---
+++ Python/rockpaperscissors.py
@@ -0,0 +1,14 @@
+import random
+a=['Rock','Paper','Scissors']
+print("Enter your choice")
+inp=input()
+res=random.choice(a)
+print("Computer Choice is: ",res)
+print("THEREFORE:")
+if(res==inp):
+    print("DRAW")
+else:
+    if((res.upper()=='ROCK' and inp.upper()=='PAPER') or (res.upper()=='SCISSORS' and inp.upper()=='ROCK') or (res.upper()=='PAPER' and inp.upper()=='SCISSORS')):
+        print("YOU WIN")
+    else:
+        print("YOU LOSE")
