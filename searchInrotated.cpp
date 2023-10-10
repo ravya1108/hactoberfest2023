3d77bfa91c0ef5e3b37b7e7bfc4ac8dcfe58e236
Tushar <98016057+tushar453@users.noreply.github.com>
2023-10-10 15:39:10 +0530

    Create searchInrotated.cpp
    search element in rotated array @hactoberfest2023 #175

---
+++ searchInrotated.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+int searchInRotatedArray(const std::vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        // Check which side of the pivot the mid element is on
+        if (nums[left] <= nums[mid]) {
+            // Left half is sorted
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            // Right half is sorted
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1; // Element not found
+}
+
+int main() {
+    std::vector<int> rotatedArray = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+
+    int result = searchInRotatedArray(rotatedArray, target);
+
+    if (result != -1) {
+        std::cout << "Element " << target << " found at index " << result << std::endl;
+    } else {
+        std::cout << "Element " << target << " not found in the array." << std::endl;
+    }
+
+    return 0;
+}
