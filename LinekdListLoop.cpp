c3f11cd94ca26e79d2b77362df92503619a7791c
ArshiyaArora <141601542+ArshiyaArora@users.noreply.github.com>
2023-10-24 06:41:28 +0530

    Create LinekdListLoop.cpp

---
+++ LinekdListLoop.cpp
@@ -0,0 +1,59 @@
+// C++ program to detect loop in a linked list
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/* Link list node */
+class Node {
+public:
+    int data;
+    Node* next;
+};
+ 
+void push(Node** head_ref, int new_data)
+{
+    /* allocate node */
+    Node* new_node = new Node();
+ 
+    /* put in the data */
+    new_node->data = new_data;
+ 
+    /* link the old list of the new node */
+    new_node->next = (*head_ref);
+ 
+    /* move the head to point to the new node */
+    (*head_ref) = new_node;
+}
+ 
+int detectLoop(Node* list)
+{
+    Node *slow_p = list, *fast_p = list;
+ 
+    while (slow_p && fast_p && fast_p->next) {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+        if (slow_p == fast_p) {
+            return 1;
+        }
+    }
+    return 0;
+}
+ 
+/* Driver code*/
+int main()
+{
+    /* Start with the empty list */
+    Node* head = NULL;
+ 
+    push(&head, 20);
+    push(&head, 4);
+    push(&head, 15);
+    push(&head, 10);
+ 
+    /* Create a loop for testing */
+    head->next->next->next->next = head;
+    if (detectLoop(head))
+        cout << "Loop Found";
+    else
+        cout << "No Loop";
+    return 0;
+}
